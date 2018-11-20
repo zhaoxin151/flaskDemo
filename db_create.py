bd95779c032cde2e3d6d325024309ff343840070
wanghaibo <wanghaibo@naton.cn>
2018-11-20 09:21:30 +0800

    测试更新数据库

--- db_create.py
+++ db_create.py
@@ -5,9 +5,8 @@ app = create_app('default')
 app_context = app.app_context()
 app_context.push()
 
-user = User.query.filter_by(username='zhaoxin').first()
-user.confirmed = True
-db.session.refresh(user)
+user = User.query.filter_by(username='wanghaibo123')
+user.update({'confirmed': True})
 db.session.commit()
 
 
